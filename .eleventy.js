7f157e675652b0dbb65a2696533136928f770512
Jordan Shurmer <jshurmer@scrippsnetworks.com>
2019-04-01 09:25:06 -0400

    Add Favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,26 @@
 module.exports = function (eleventyConfig) {
-    // copy "static" as is
+    // Satic things
     eleventyConfig.addPassthroughCopy("static");
     eleventyConfig.addPassthroughCopy("legero.js");
+
+    eleventyConfig.addPassthroughCopy("android-chrome-192x192.png");
+    eleventyConfig.addPassthroughCopy("android-chrome-512x512.png");
+    eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
+    eleventyConfig.addPassthroughCopy("favicon-16x16.png");
+    eleventyConfig.addPassthroughCopy("favicon-32x32.png");
+    eleventyConfig.addPassthroughCopy("site.webmanifest");
+
 
     // a TOC
     // {{ content | toc | safe }}
     const toc = require('eleventy-plugin-toc');
     eleventyConfig.addPlugin(toc);
 
+
     // Markdown (including adding IDs to the headers)
     const markdownIt = require('markdown-it');
     const markdownItAnchor = require('markdown-it-anchor');
+
 
     // Date filter
     // https://www.npmjs.com/package/nunjucks-date
