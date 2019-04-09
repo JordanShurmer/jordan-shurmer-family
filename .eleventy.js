92cfcc464739ee8c886f38874156961188aad394
Jordan Shurmer <jshurmer@scrippsnetworks.com>
2019-04-09 14:45:37 -0400

    use the new eleventy-plugin-nesting-toc plugin

--- .eleventy.js
+++ .eleventy.js
@@ -13,13 +13,20 @@ module.exports = function (eleventyConfig) {
 
     // a TOC
     // {{ content | toc | safe }}
-    const toc = require('eleventy-plugin-toc');
+    const toc = require('eleventy-plugin-nesting-toc');
     eleventyConfig.addPlugin(toc);
 
 
     // Markdown (including adding IDs to the headers)
     const markdownIt = require('markdown-it');
     const markdownItAnchor = require('markdown-it-anchor');
+    eleventyConfig.setLibrary("md",
+        markdownIt({
+            html: true,
+            linkify: true,
+            typographer: true,
+        }).use(markdownItAnchor, {})
+    );
 
 
     // Date filter
@@ -29,13 +36,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addFilter('date', nunjucksDate);
 
 
-    eleventyConfig.setLibrary("md",
-        markdownIt({
-            html: true,
-            linkify: true,
-            typographer: true,
-        }).use(markdownItAnchor, {})
-    );
 
     return {
         jsDataFileSuffix: ".metadata",
